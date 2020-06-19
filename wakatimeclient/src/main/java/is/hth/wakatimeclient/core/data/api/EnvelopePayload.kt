3447c945c99990501eff206ae75d6b071c480037
Hrafn <hrafn@hth.is>
2020-06-19 13:42:01 +0000

    - EnvelopePayload.kt created

---
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/core/data/api/EnvelopePayload.kt
@@ -0,0 +1,15 @@
+package `is`.hth.wakatimeclient.core.data.api
+
+/**
+ * An annotation for identifying the payload that we want to extract from an API response wrapped in
+ * an envelope object.
+ *
+ * Based on Google's EnvelopPayload class from the Plaid sample application
+ */
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.PROPERTY_SETTER
+)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+annotation class EnvelopePayload(val value: String = "")
