920a099c6369e6974a358cd35e947e67f18489c5
Hrafn <hrafn@hth.is>
2020-07-05 16:39:30 +0000

    - ProcessResult removed

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/core/ProcessResult.kt
+++
@@ -1,8 +0,0 @@
-package `is`.hth.wakatimeclient.core
-
-sealed class ProcessResult<out T> {
-
-    data class Success<out T>(val data: T): ProcessResult<T>()
-
-    data class Error(val exception: Exception): ProcessResult<Nothing>()
-}
