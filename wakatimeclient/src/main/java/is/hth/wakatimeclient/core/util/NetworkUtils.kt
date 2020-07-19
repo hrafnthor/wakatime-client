85debf9aec8ff822c48407b613bfafd1aabd62a6
Hrafn <hrafn@hth.is>
2020-07-19 16:02:59 +0000

    - Network utils refactored

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/core/util/NetworkUtils.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/core/util/NetworkUtils.kt
@@ -1,20 +1,34 @@
 package `is`.hth.wakatimeclient.core.util
 
-import `is`.hth.wakatimeclient.core.ProcessResult
-import java.io.IOException
-
+import `is`.hth.wakatimeclient.core.data.Error
+import `is`.hth.wakatimeclient.core.data.Results
+import retrofit2.Response
 
 /**
- * Wrap a suspending API [call] in try/catch. In case an exception is thrown, a [Result.Error] is
- * created based on the [errorMessage].
+ * Structured execution and result handling for network operations
  */
-suspend fun <T : Any> safeApiCall(
-    call: suspend () -> ProcessResult<T>,
-    errorMessage: String
-): ProcessResult<T> {
-    return try {
-        call()
-    } catch (e: Exception) {
-        ProcessResult.Error(IOException(errorMessage, e))
+suspend fun <T : Any, R : Any> networkOperation(
+    /**
+     * Execute the network operation
+     */
+    load: suspend () -> Response<T>,
+    /**
+     * Perform any required payload conversion before delivering the final results
+     */
+    convert: (T) -> R,
+    /**
+     * In case the response does not indicate success attempt
+     * to convert the response's code to an [Error]
+     */
+    error: (Int) -> Error
+): Results<R> {
+    val response = load()
+    return when {
+        response.isSuccessful -> {
+            response.body()?.let {
+                Results.Values(convert(it))
+            } ?: Results.Empty()
+        }
+        else -> Results.Failure(error(response.code()))
     }
 }
