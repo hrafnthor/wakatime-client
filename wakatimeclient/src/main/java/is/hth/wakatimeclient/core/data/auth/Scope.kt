dc8b5d4c711c9eeb73eeee44c1edaa141337fe45
Hrafn <hrafn@hth.is>
2020-08-03 15:53:12 +0000

    - Minor format refactoring of Scopes

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/core/data/auth/Scope.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/core/data/auth/Scope.kt
@@ -10,13 +10,13 @@ sealed class Scope(val name: String) {
 
         fun valueOf(name: String): Scope {
             return when(name){
-                EMAIL.name -> EMAIL
-                READ_LOGGED_TIME.name -> READ_LOGGED_TIME
-                WRITE_LOGGED_TIME.name -> WRITE_LOGGED_TIME
-                READ_STATS.name -> READ_STATS
-                READ_ORGS.name -> READ_ORGS
-                READ_PRIVATE_LEADERBOARDS.name -> READ_PRIVATE_LEADERBOARDS
-                WRITE_PRIVATE_LEADERBOARDS.name -> WRITE_PRIVATE_LEADERBOARDS
+                Email.name -> Email
+                ReadLoggedTime.name -> ReadLoggedTime
+                WriteLoggedTime.name -> WriteLoggedTime
+                ReadStats.name -> ReadStats
+                ReadOrgs.name -> ReadOrgs
+                ReadPrivateLeaderboards.name -> ReadPrivateLeaderboards
+                WritePrivateLeaderboards.name -> WritePrivateLeaderboards
                 else -> throw IllegalArgumentException("Unknown scope name $name")
             }
         }
@@ -25,30 +25,30 @@ sealed class Scope(val name: String) {
     /**
      * Access user’s email and identity information
      */
-    object EMAIL: Scope("email")
+    object Email: Scope("email")
     /**
      * Access user’s coding activity and other stats.
      */
-    object READ_LOGGED_TIME : Scope("read_logged_time")
+    object ReadLoggedTime : Scope("read_logged_time")
     /**
      * Modify user’s coding activity
      */
-    object WRITE_LOGGED_TIME: Scope("write_logged_time")
+    object WriteLoggedTime: Scope("write_logged_time")
     /**
      * Access user’s languages, editors, and operating systems used
      */
-    object READ_STATS: Scope("read_stats")
+    object ReadStats: Scope("read_stats")
     /**
      * Access user’s organizations, and coding activity for dashboard members.
      */
-    object READ_ORGS: Scope("read_orgs")
+    object ReadOrgs: Scope("read_orgs")
     /**
      * Access user’s private leaderboards.
      */
-    object READ_PRIVATE_LEADERBOARDS: Scope("read_private_leaderboards")
+    object ReadPrivateLeaderboards: Scope("read_private_leaderboards")
     /**
      * Modify user’s private leaderboards, including adding/removing members when
      * current user had Admin or Owner role.
      */
-    object WRITE_PRIVATE_LEADERBOARDS: Scope("write_private_leaderboards")
+    object WritePrivateLeaderboards: Scope("write_private_leaderboards")
 }
