30a514b6e3438b09fd8a7977737b6631c0daf387
Hrafn <hrafn@hth.is>
2021-04-02 22:01:44 +0000

    - regression in library

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/core/data/net/Mime.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/core/data/net/Mime.kt
@@ -1,6 +1,6 @@
 package `is`.hth.wakatimeclient.core.data.net
 
-sealed class Mime(private val name: String) {
+sealed class Mime(val name: String) {
 
     object ApplicationJson : Mime("application/json")
 
