a7a7ad75bd743cc1fc05e4cdccbfdf66b782df5a
Hrafn Thorvaldsson <hrafn@hth.is>
2021-04-18 16:39:06 +0000

    - move transformer out of class

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/wakatime/data/api/ServiceError.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/wakatime/data/api/ServiceError.kt
@@ -13,36 +13,35 @@ data class ServiceError(
     @SerialName("error")
     val message: String = "",
     @SerialName("errors")
-    @Serializable(Transformer::class)
+    @Serializable(FieldErrorTransformer::class)
     val fieldErrors: List<FieldError> = emptyList()
-) {
-
-    /**
-     * Performs modifications on the incoming 'errors' object, breaking it apart into
-     * separate [FieldError]s
-     */
-    object Transformer : JsonTransformingSerializer<List<FieldError>>(
-        ListSerializer(FieldError.serializer())
-    ) {
-        override fun transformDeserialize(element: JsonElement): JsonElement {
-            return when (element) {
-                is JsonObject -> JsonArray(element.mapTo(mutableListOf()) {
-                    val description = if (it.value is JsonArray) {
-                        it.value.jsonArray.firstOrNull()?.jsonPrimitive?.content ?: ""
-                    } else ""
-                    buildJsonObject {
-                        put("name", it.key)
-                        put("description", description)
-                    }
-                })
-                else -> super.transformDeserialize(element)
-            }
-        }
-    }
-}
+)
 
 @Serializable
 data class FieldError(
     val name: String,
     val description: String
 )
+
+/**
+ * Performs modifications on the incoming 'errors' object, breaking it apart into
+ * separate [FieldError]s
+ */
+internal object FieldErrorTransformer : JsonTransformingSerializer<List<FieldError>>(
+    ListSerializer(FieldError.serializer())
+) {
+    override fun transformDeserialize(element: JsonElement): JsonElement {
+        return when (element) {
+            is JsonObject -> JsonArray(element.mapTo(mutableListOf()) {
+                val description = if (it.value is JsonArray) {
+                    it.value.jsonArray.firstOrNull()?.jsonPrimitive?.content ?: ""
+                } else ""
+                buildJsonObject {
+                    put("name", it.key)
+                    put("description", description)
+                }
+            })
+            else -> super.transformDeserialize(element)
+        }
+    }
+}
