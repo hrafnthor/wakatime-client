107438e1d5f41853c66abc7ced83e2e97c3ba45e
Hrafn Thorvaldsson <hrafn@hth.is>
2021-04-22 13:34:56 +0000

    - added utility method for extracting json fields

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/core/Utils.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/core/Utils.kt
@@ -2,6 +2,8 @@ package `is`.hth.wakatimeclient.core
 
 import `is`.hth.wakatimeclient.core.data.ErrorProcessor
 import `is`.hth.wakatimeclient.core.data.Results
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonObjectBuilder
 
 /**
  * Wrap a suspending operation in try/catch. In case an exception is thrown,
@@ -16,4 +18,26 @@ inline fun <T : Any> safeOperation(
     operation()
 } catch (e: Exception) {
     Results.Failure(processor.onError(e))
+}
+
+/**
+ * Convenience method for extracting a potential value from a JsonObject and
+ * adding it to a new builder
+ * @param builder that is being used to construct a new JsonObject
+ * @param element from which fields are being parsed from
+ * @param key for the element that is being looked for
+ * @param default called when the element is not found and whatever default step should be
+ * performed. Is passed the previously supplied key
+ */
+internal inline fun findValue(
+    builder: JsonObjectBuilder,
+    element: JsonObject,
+    key: String,
+    default: (String) -> Unit
+) {
+    element[key]?.let {
+        builder.put(key, it)
+    } ?: run {
+        default(key)
+    }
 }
