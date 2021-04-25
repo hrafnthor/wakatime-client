15005bd6ce9e667babd058d0337961d312d8a7d7
Hrafn Thorvaldsson <hrafn@hth.is>
2021-04-25 15:49:36 +0000

    - added juni5 mannodermaus plugin - removed still lingering version properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,14 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.7.1.1")
     }
 }
 
 plugins {
+    // Used for dependency update checking
     id("com.github.ben-manes.versions") version "0.38.0"
+    // Native Kotlin Serialization
     kotlin("plugin.serialization") version "1.4.31"
 }
 
