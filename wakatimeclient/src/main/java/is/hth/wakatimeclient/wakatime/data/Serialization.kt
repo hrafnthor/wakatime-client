b6c65cd8f8d556a7c20f67763ef1db7b1f01203a
Hrafn Thorvaldsson <hrafn@hth.is>
2021-05-22 19:06:11 +0000

    - serialization value extraction utilities made more robust

--- wakatimeclient/src/main/java/is/hth/wakatimeclient/wakatime/data/Serialization.kt
+++ wakatimeclient/src/main/java/is/hth/wakatimeclient/wakatime/data/Serialization.kt
@@ -8,51 +8,75 @@ import kotlinx.serialization.json.*
  *
  * @param builder that is being used to construct a new JsonObject
  * @param element from which fields are being parsed from
- * @param key for the element that is being looked for
+ * @param sourceKey for the field that is being looked for in the supplied element
+ * @param destKey to use for storing the found value into the builder
  * @param default called when the element is not found, or the value is null, and whatever default step should be
  * performed. Is passed the previously supplied key
  */
 internal inline fun findValue(
     builder: JsonObjectBuilder,
     element: JsonObject,
-    key: String,
-    default: (String) -> Unit
+    sourceKey: String,
+    destKey: String,
+    default: () -> JsonElement
 ) {
-    val innerElement = element[key]
-    if (innerElement != null && innerElement != JsonNull) {
-        builder.put(key, innerElement)
-    } else default(key)
+    val innerElement = element[sourceKey]?.let {
+        if (it == JsonNull) {
+            default()
+        } else it
+    } ?: default()
+    builder.put(destKey, innerElement)
 }
 
-internal fun findValue(
-    builder: JsonObjectBuilder,
-    element: JsonObject,
-    key: String,
-    default: Boolean
-) {
-    findValue(builder, element, key) {
-        builder.put(key, default)
-    }
-}
-
-internal fun findValue(
-    builder: JsonObjectBuilder,
-    element: JsonObject,
-    key: String,
-    default: String
-) {
-    findValue(builder, element, key) {
-        builder.put(key, default)
-    }
-}
-
-internal fun findValue(
-    builder: JsonObjectBuilder,
+/**
+ * Convenience method for extracting and setting a value to the builder
+ *
+ * @param element to extract the value from
+ * @param key to use for the extraction and placement in the builder
+ * @param default value to use in case no value is found in the supplied element with the given key
+ */
+internal fun JsonObjectBuilder.findValue(
     element: JsonObject,
     key: String,
     default: Int
-) {
-    findValue(builder, element, key) {
-        builder.put(key, default)
-    }
-}
+): Unit = findValue(this, element, key, key) { JsonPrimitive(default) }
+
+/**
+ * Convenience method for extracting and setting a value to the builder
+ *
+ * @param element to extract the value from
+ * @param key to use for the extraction and placement in the builder
+ * @param default value to use in case no value is found in the supplied element with the given key
+ */
+internal fun JsonObjectBuilder.findValue(
+    element: JsonObject,
+    key: String,
+    default: Boolean
+): Unit = findValue(this, element, key, key) { JsonPrimitive(default) }
+
+/**
+ * Convenience method for extracting and setting a value to the builder
+ *
+ * @param element to extract the value from
+ * @param key to use for the extraction and placement in the builder
+ * @param default value to use in case no value is found in the supplied element with the given key
+ */
+internal fun JsonObjectBuilder.findValue(
+    element: JsonObject,
+    key: String,
+    default: String
+): Unit = findValue(this, element, key, key) { JsonPrimitive(default) }
+
+/**
+ * Convenience method for extracting and setting a value to the builder
+ *
+ * @param element to extract the value from
+ * @param key to use for the extraction and placement in the builder
+ * @param default value to use in case no value is found in the supplied element with the given key
+ */
+internal inline fun JsonObjectBuilder.findValue(
+    element: JsonObject,
+    key: String,
+    default: () -> JsonElement
+): Unit = findValue(this, element, key, key, default)
+
