009cd75db03e1979658f4971a3ed1464ee67a8f1
Hrafn Thorvaldsson <hrafn@hth.is>
2021-05-23 17:08:13 +0000

    - re-added fragmentx as dependency, as it is required for the use of ActivityResultsLauncher

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
         //#region AndroidX UI
         val androidxActivityVersion: String by project
         api("androidx.activity:activity-ktx:$androidxActivityVersion")
+        // fragment required for the use of ActivityResultLauncher
+        val androidxFragmentVersion: String by project
+        api("androidx.fragment:fragment-ktx:$androidxFragmentVersion")
         //#endregion
 
         //#region AndroidX Lifecycle
